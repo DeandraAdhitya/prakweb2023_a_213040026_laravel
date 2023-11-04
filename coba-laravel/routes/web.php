48bbbc5b9b3cfc88b28d5f3caba8f28b30111723
deandraaditya <deandra.213040026@mail.unpas.ac.id>
2023-11-05 00:03:27 +0700

    Video 5. Model, Collection & Controller

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,55 +31,10 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Deandra Adhitya",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repellendus quae doloribus dignissimos tempora saepe debitis fuga commodi veniam sunt quia tenetur cumque, soluta labore assumenda ducimus, deleniti quos quisquam, velit ea suscipit? Deserunt officiis eaque inventore sapiente dolores voluptates ipsam recusandae accusantium in quas dolor omnis ipsa dolorem eius mollitia a temporibus qui placeat, distinctio voluptate quae eum? Autem animi, dolores sapiente ipsa illum sequi doloremque voluptate eaque obcaecati non? Ipsum odit ab veniam velit neque error sed delectus?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Yuji",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repellendus quae doloribus dignissimos tempora saepe debitis fuga commodi veniam sunt quia tenetur cumque, soluta labore assumenda ducimus, deleniti quos quisquam, velit ea suscipit? Deserunt officiis eaque inventore sapiente dolores voluptates ipsam recusandae accusantium in quas dolor omnis ipsa dolorem eius mollitia a temporibus qui placeat, distinctio voluptate quae eum? Autem animi, dolores sapiente ipsa illum sequi doloremque voluptate eaque obcaecati non? Ipsum odit ab veniam velit neque error sed delectus?"
-        ],
-    ];
+Route::get('/blog', [PostController::class, 'index']);
 
-    return view('posts' , [
-        "title" => "Posts",
-        "posts"=> $blog_posts
-    ]);
-});
+
 
 
 //Halaman Single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Deandra Adhitya",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repellendus quae doloribus dignissimos tempora saepe debitis fuga commodi veniam sunt quia tenetur cumque, soluta labore assumenda ducimus, deleniti quos quisquam, velit ea suscipit? Deserunt officiis eaque inventore sapiente dolores voluptates ipsam recusandae accusantium in quas dolor omnis ipsa dolorem eius mollitia a temporibus qui placeat, distinctio voluptate quae eum? Autem animi, dolores sapiente ipsa illum sequi doloremque voluptate eaque obcaecati non? Ipsum odit ab veniam velit neque error sed delectus?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Yuji",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repellendus quae doloribus dignissimos tempora saepe debitis fuga commodi veniam sunt quia tenetur cumque, soluta labore assumenda ducimus, deleniti quos quisquam, velit ea suscipit? Deserunt officiis eaque inventore sapiente dolores voluptates ipsam recusandae accusantium in quas dolor omnis ipsa dolorem eius mollitia a temporibus qui placeat, distinctio voluptate quae eum? Autem animi, dolores sapiente ipsa illum sequi doloremque voluptate eaque obcaecati non? Ipsum odit ab veniam velit neque error sed delectus?"
-        ],
-    ];
-    
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
