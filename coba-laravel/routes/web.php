565f9460062497b3d4db02ba18f5b59c3979119b
deandraaditya <deandra.213040026@mail.unpas.ac.id>
2023-11-12 20:25:35 +0700

    Video 12. Redesign UI

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -20,13 +20,15 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('home' , [
-        "title" => "Home"
+        "title" => "Home",
+        'active' => 'home',
     ]);
 });
 
 Route::get('/about', function () {
     return view('about' , [
         "title" => "About",
+        'active' => 'about',
         "name" => "Deandra Adhitya",
         "email" => "deandraaditya45@gmail.com",
         "image" => "icon.png"
@@ -39,6 +41,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => "Post Categories",
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -46,6 +49,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post by Category: $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author')
     ]);
 });
