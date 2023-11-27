621505b9141755b968a5177f6bf1e2182d0f76da
deandraaditya <deandra.213040026@mail.unpas.ac.id>
2023-11-27 07:51:18 +0700

    Video 18. Create Post Form

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -57,4 +57,5 @@ Route::get('/dashboard', function () {
     return view('dashboard.index');
 })->middleware('auth');
 
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
